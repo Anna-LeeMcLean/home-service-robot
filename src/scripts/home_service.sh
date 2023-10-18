ce7b460ea0af1fef005c4afe0687ffa2b5e4310a
Anna-Lee McLean <mcleanannalee@gmail.com>
2023-10-17 20:42:16 -0700

    Updated CMakeLists.txt to include new node. Update shell scripts to run the set_initial_position node before launching rviz and killing it after rviz has has launched.

--- src/scripts/home_service.sh
+++ src/scripts/home_service.sh
@@ -4,9 +4,17 @@ xterm -e "roslaunch turtlebot3_gazebo turtlebot3_world.launch" &
 
 sleep 5
 
+xterm -e "rosrun rviz_launchers set_robot_initial_position" &
+
+sleep 5
+
 xterm -e "roslaunch rviz_launchers view_navigation.launch" &
 
-sleep 15
+sleep 5
+
+xterm -e "rosnode kill set_robot_initial_position" &
+
+sleep 5
 
 xterm -e "rosrun add_markers add_markers" &
 
--- src/scripts/test_navigation.sh
+++ src/scripts/test_navigation.sh
@@ -4,5 +4,13 @@ xterm -e "roslaunch turtlebot3_gazebo turtlebot3_world.launch" &
 
 sleep 5
 
+xterm -e "rosrun rviz_launchers set_robot_initial_position" &
+
+sleep 5
+
 xterm -e "roslaunch turtlebot3_navigation turtlebot3_navigation.launch" &
+
+sleep 5
+
+xterm -e "rosnode kill set_robot_initial_position" &
 
